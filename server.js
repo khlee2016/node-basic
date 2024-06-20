7721c93c3909d417cd0653ea9902a35e6b6ac0fa
khlee2016 <khlee@seoultech.ac.kr>
2024-06-20 20:59:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-//Load HTTP module
-var http = require("http");
+const express = require('express')
+const app = express()
 const port = 8080
-//Create HTTP server and listen on port 8000 for requests
-http.createServer(function (request, response) {
-   console.log('Request:' + request.socket.remoteAddress)
-   // Set the response HTTP header with HTTP status and Content type
-   response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-   // Send the response body "Hello World"
-   currdate = new Date().toLocaleString("ko-KR", {timeZone: "Asia/Seoul"});
-   //response.end('Hello World ' + currdate.toLocaleString() + '\n');
-   response.end('Hello World ' + currdate + '\n');
-}).listen(port);
-// Print URL for accessing server
-console.log('Server running at http://127.0.0.1:' + port)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/about', function (request, response) {
+        response.send('About Us');
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
